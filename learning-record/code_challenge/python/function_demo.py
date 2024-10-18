b1c177739e4941f972aaa017d6639ae8b283738c
James Cheng <391323163@qq.com>
2024-10-18 16:53:07 +0800

    1

--- learning-record/code_challenge/python/function_demo.py
+++ learning-record/code_challenge/python/function_demo.py
@@ -1,5 +1,6 @@
 import string
 import random
+from typing import List
 
 def calculate(int1, int2):
     return int1 + int2
@@ -68,5 +69,20 @@ print(result2)
 
 def maxTest(num1: int, num2: int):
     return max(num1, num2)
-
 print(maxTest(19,91))
+
+
+def divisionTest(num1: int, num2: int) -> string:
+    quotient = num1 // num2
+    remainder = num1 % num2
+    return str(quotient) + "..." + str(remainder)
+print(divisionTest(31, 10))
+
+def nodeTest(l1: List, l2: List):
+    while l1 and l2:
+        print(l1, l2)
+        del l1[0] 
+        del l2[0]
+l1 = [1,2,3]
+l2 = [4,5]
+nodeTest(l1, l2)
--- learning-record/code_challenge/python/leetcode/buy_and_sell_stock.py
+++ learning-record/code_challenge/python/leetcode/buy_and_sell_stock.py
@@ -24,6 +24,7 @@ class Solution:
                 buy_price = price 
             max_profit = max(max_profit, price - buy_price)
         return max_profit
+    
 solution = Solution()
 prices = [7,1,5,3,6,4]
 result = solution.maxProfit(prices)
