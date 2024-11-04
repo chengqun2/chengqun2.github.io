e3d5f8c4966aa7aa662d202944832c5338dcc768
James Cheng <391323163@qq.com>
2024-11-04 10:36:56 +0800

    1

--- learning-record/code_challenge/python/spider/crawl_airport_code.py
+++ learning-record/code_challenge/python/spider/crawl_airport_code.py
@@ -9,7 +9,6 @@ worksheet = workbook.add_worksheet()
 worksheet.write_row(0, 0, header)
 # crawling logic... 
 base_url = "https://airportcode.bmcx.com/"
-row_number = 0
 for i in range(0, 289):
     url = "".join([base_url,f'{i+1}__airportcode'])
     response = requests.get(url)
@@ -19,6 +18,5 @@ for i in range(0, 289):
     for j in range(1, len(tr_data)):
         td_data = tr_data[j].select("td")
         row = td_data[1].text.strip(),td_data[3].text.strip()
-        print(i*30+j, 0, row)
         worksheet.write_row(i*30+j, 0, row)
 workbook.close()
