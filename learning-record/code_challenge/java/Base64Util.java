6bc9d68627d249d3048c3c1612a870a5d29fc066
James Cheng <391323163@qq.com>
2024-11-05 09:51:49 +0800

    1

---
+++ learning-record/code_challenge/java/Base64Util.java
@@ -0,0 +1,23 @@
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+public class Base64Util {
+    public static void main(String[] args){
+       System.out.println(Base64Util.setEncryptionBase64("320902199005220019"));
+    }
+
+    public static String setEncryptionBase64(String str) {
+        byte[] b = null;
+        String s = null;
+        try {
+            b = str.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        if (b != null) {
+            Base64.Encoder encoder = Base64.getMimeEncoder();
+            s = encoder.encodeToString(b);
+        }
+        return s;
+    }
+}
